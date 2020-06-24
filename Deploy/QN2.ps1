6bb364b6bcef6f43c0c62dd9f1f1005e405669f5
m.avsek94 <m.avsek94@gmail.com>
2020-06-24 23:51:27 +0000

    final ps script
    Signed-off-by: m.avsek94 <m.avsek94@gmail.com>

--- Deploy/QN2.ps1
+++ Deploy/QN2.ps1
@@ -5,7 +5,7 @@
 
 try{
     docker stop $(docker ps -aq)
-    docker pull avsek12/trcweb | docker rm $(docker ps -aq) |  docker run --name trcweb -dp 80:80 avsek12/trcweb }
+    docker pull avsek12/trc_assessment:1.0.0 | docker rm $(docker ps -aq) |  docker run --name trcweb -dp 80:80 avsek12/trc_assessment:1.0.0 }
        
         catch{
         Write-Host $_.Exception.Message 
